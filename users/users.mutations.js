192ed731d176ca7a454f1f84ecf80a33c87ff52a
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-11 14:21:25 +0900

    5-2 Mutation ( createAccount) 생성
    1. 계정생성하는 mutation 생성
    
    2. prisma는 promise를 return 해 async와 await을 사용
    
    3. filter 연산자 (OR) 로
    unique한 값으로 설정해 놓은 userName과 emil이 exist할 경우를 대비한 코드 작성

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,11 +1,17 @@
+import client from "../client";
 export default {
-    Mutation: {
-        createAccount:{_,{
-            firstName,
-    lastName,
-    userName,
-    email,
-    password,
-        }}
-    }
-}
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, userName, email, password }
+    ) => {
+      //1. 동일한 username, emial이  DB 에 있는지
+      const existingUser = await client.user.findfirst({
+        where: {
+          OR: [{ userName }, { email }],
+        },
+      });
+      //2. 없으면 비번 hash해서 계정생성 / 있으면 return
+    },
+  },
+};
