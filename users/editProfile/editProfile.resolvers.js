055eac9e464671aac862fa80b3cb536b36b94c20
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-11 19:17:47 +0900

    5-9 editProfile 구현
    1. hash 해야할 password 부분을 const(상수)로 지정하면 read-only 오류걸림
    
    2. let(변수)로 지정해놔야함
    
    3. 수정된 password를 다시한번 hash해줘야함

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,38 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
   Mutation: {
-    editProfile: () => console.log("hi"),
+    editProfile: async (
+      _,
+      { firstname, lastName, userName, email, password: newPassword }
+    ) => {
+      let passwordHash = null; // const로 하면 read-only가 된다
+      if (newPassword) {
+        passwordHash = await bcrypt.hash(newPassword, 10);
+      }
+      const updateUser = await client.user.update({
+        where: {
+          id: 4,
+        },
+        data: {
+          firstname,
+          lastName,
+          userName,
+          email,
+          ...(passwordHash && { password: passwordHash }),
+        },
+      });
+      if (updateUser.id) {
+        return {
+          ok: true,
+        };
+      } else {
+        return {
+          ok: false,
+          error: "프로필을 수정할 수 없습니다",
+        };
+      }
+    },
   },
 };
