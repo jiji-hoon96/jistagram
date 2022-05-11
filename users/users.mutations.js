ed229c8eb6af3bbacf9b2b481d65964b51519fde
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-11 14:52:38 +0900

    5-3 bcrypt 를 이용해 password를 hash

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,6 @@
 import client from "../client";
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
     createAccount: async (
@@ -6,9 +8,20 @@ export default {
       { firstName, lastName, userName, email, password }
     ) => {
       //1. 동일한 username, emial이  DB 에 있는지
-      const existingUser = await client.user.findfirst({
+      const existingUser = await client.user.findFirst({
         where: {
           OR: [{ userName }, { email }],
+        },
+      });
+      console.log(existingUser);
+      const uglyPassword = await bcrypt.hash(password, 10);
+      return client.user.create({
+        data: {
+          userName,
+          email,
+          firstName,
+          lastName,
+          password: uglyPassword,
         },
       });
       //2. 없으면 비번 hash해서 계정생성 / 있으면 return
