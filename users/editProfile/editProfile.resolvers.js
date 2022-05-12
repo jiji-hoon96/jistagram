3f8e7903b64b51719b4c652fcf9cfdf44260f6ea
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-12 18:21:33 +0900

    5-12 이중함수부분 수정

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,42 +1,40 @@
 import bcrypt from "bcrypt";
 import client from "../../client";
-import { protectResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  { loggedInUser }
+) => {
+  let passwordHash = null;
+  if (newPassword) {
+    passwordHash = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(passwordHash && { password: passwordHash }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+};
 export default {
   Mutation: {
-    editProfile: protectResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser }
-      ) => {
-        let passwordHash = null;
-        if (newPassword) {
-          passwordHash = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(passwordHash && { password: passwordHash }),
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not update profile.",
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
