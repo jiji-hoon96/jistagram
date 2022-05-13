00d68db61f63939e68269c2d125e1d105fe4df2f
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-13 18:55:06 +0900

    7-5 전체 팔로워 구하기 & 본인계정로그인 됫는지 확인

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -13,5 +13,10 @@ export default gql`
     avatar: String
     following: [User]
     followers: [User]
+    totalFollowing: Int!
+    totalFollowers: Int!
+    isMe: Boolean!
   }
 `;
+
+//isFollowing: Boolean!
