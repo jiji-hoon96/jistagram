6cfba024550733bb3b9db29844adf540d799c853
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-14 19:42:09 +0900

    8-3 hashtag id로 확인하기

--- photos/photos.typeDefs.js
+++ photos/photos.typeDefs.js
@@ -1,21 +1,20 @@
 import { gql } from "apollo-server";
 
 export default gql`
-    type Photo {
-        id: String!
-        user: String!
-        createdAt: String!
-        file : String!
-        updatedAt: String!
-        userId: String!
-        caption : String
-        hashtags : [Hashtag]
-    }
-    type Hashtag {
-        id: String!
-        Hashtag : String!
-        photos:  [Photo]
-        createdAt: String!
-        updatedAt :String!
-    }
-`
+  type Photo {
+    id: Int!
+    user: User!
+    file: String!
+    caption: String
+    hashtags: [Hashtag]
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Hashtag {
+    id: Int!
+    hashtag: String!
+    photos: [Photo]
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
