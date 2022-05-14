cfe58ca1e36ef2cc5fd425cd01dadae5ca5687ac
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-14 19:12:40 +0900

    8-2 connectOrCreate 이용해 hash생성

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,12 +1,30 @@
+import client from '../../client';
 import {protectedResolver} from '../../users/users.utils'
 export default {
     Mutation : {
         uploadPhoto: protectedResolver(async (_, {file,caption}, {loggedInUser})=>{
+            let hashtagObj =[];
             if(caption){
                 //해쉬테그가 있거나 , 만들거면 여기서 작업
                 const hashtags = caption.match(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g);
-                console.log(hashtags)
+                hashtagObj = hashtags.map(hashtag => ({where: {hashtag}, create: {hashtag}}))
             }
+            return client.photo.create({
+                data:{
+                    file,
+                    caption,
+                    user: {
+                        connect:{
+                            id:loggedInUser.id
+                        }
+                    },
+                    ...(hashtagObj.length > 0 && {
+                        hashtags:{
+                            connectOrCreate: hashtagObj
+                        }
+                    })
+                }
+            })
             //그 후에 저장
         }
         )
