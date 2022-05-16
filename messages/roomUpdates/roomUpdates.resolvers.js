b15f01d50bb2d365094c80af34cbf361a3671530
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-16 22:26:08 +0900

    12-4 backend 마무리

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,14 +1,20 @@
-import { withFilter } from "apollo-server";
+import { withFilter } from "apollo-server-express";
 import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
+
 export default {
   Subscription: {
     roomUpdates: {
       subscribe: async (root, args, context, info) => {
-        const room = await client.room.findUnique({
+        const room = await client.room.findFirst({
           where: {
             id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
           },
           select: {
             id: true,
@@ -19,8 +25,26 @@ export default {
         }
         return withFilter(
           () => pubsub.asyncIterator(NEW_MESSAGE),
-          ({ roomUpdates }, { id }) => {
-            return roomUpdates.roomId === id;
+          async ({ roomUpdates }, { id }, { loggedInUser }) => {
+            if (roomUpdates.roomId === id) {
+              const room = await client.room.findFirst({
+                where: {
+                  id,
+                  users: {
+                    some: {
+                      id: loggedInUser.id,
+                    },
+                  },
+                },
+                select: {
+                  id: true,
+                },
+              });
+              if (!room) {
+                return false;
+              }
+              return true;
+            }
           }
         )(root, args, context, info);
       },
--- messages/roomUpdates/roomUpdates.typeDefs.js
+++ messages/roomUpdates/roomUpdates.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Subscription {
-    roomUpdates(id: Int!): Message
+    roomUpdates(id:Int!): Message
   }
 `;
