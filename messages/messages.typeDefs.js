21bab06b806b4321a92eeee017d2d301becd718a
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-16 10:47:26 +0900

    11-1 message만들기
    1. schema 모델 정리
    
    2. seeRoom 구현

---
+++ messages/messages.typeDefs.js
@@ -0,0 +1,18 @@
+import { gql } from "apollo-server";
+export default gql`
+    type Message{
+        id: Int!
+        createdAt: String!
+        updateAt: String!
+        payload: String!
+        user: User!
+        room: Room!
+    }
+    type Room{
+        id: Int!
+        createdAt: String!
+        updateAt: String!
+        users: [User]
+        messages: [Message]
+    }
+`
