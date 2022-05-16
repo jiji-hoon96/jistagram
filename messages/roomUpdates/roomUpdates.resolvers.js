63e8fbb49f06c14dccab9108c5abae88ff65db54
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-16 17:24:12 +0900

    12- 필터링 오류 수정
    1. 아이디가 일치하지 않으면 볼 수 없도록 설정
    
    2. 이중함수

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -4,14 +4,27 @@ import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
-  Subscription: {
-    roomUpdates: {
-      subscribe: withFilter(
-        () => pubsub.asyncIterator(NEW_MESSAGE),
-        ({ roomUpdates }, { id }) => {
-          return roomUpdates.roomId === id;
-        }
-      ),
+    Subscription: {
+      roomUpdates: {
+        subscribe: async (root, args, context, info) => {
+          const room = await client.room.findUnique({
+            where: {
+              id: args.id,
+            },
+            select: {
+              id: true,
+            },
+          });
+          if (!room) {
+            throw new Error("You shall not see this.");
+          }
+          return withFilter(
+            () => pubsub.asyncIterator(NEW_MESSAGE),
+            ({ roomUpdates }, { id }) => {
+              return roomUpdates.roomId === id;
+            }
+          )(root, args, context, info);
+        },
+      },
     },
-  },
-};
+  };
