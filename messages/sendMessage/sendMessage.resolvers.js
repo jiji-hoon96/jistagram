2e89f2573ccb23e4a132c07eb6ea83f9f392e092
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-05-16 17:06:45 +0900

    11-7 리얼타임설정, 오류해결

--- messages/sendMessage/sendMessage.resolvers.js
+++ messages/sendMessage/sendMessage.resolvers.js
@@ -1,44 +1,78 @@
 import client from "../../client";
+import { NEW_MESSAGE } from "../../constants";
+import pubsub from "../../pubsub";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
   Mutation: {
-    readMessage: protectedResolver(async (_, { id }, { loggedInUser }) => {
-      const message = await client.message.findFirst({
-        where: {
-          id,
-          userId: {
-            not: loggedInUser.id,
-          },
-          room: {
-            users: {
-              some: {
+    sendMessage: protectedResolver(
+      async (_, { payload, roomId, userId }, { loggedInUser }) => {
+        let room = null;
+        if (userId) {
+          const user = await client.user.findUnique({
+            where: {
+              id: userId,
+            },
+            select: {
+              id: true,
+            },
+          });
+          if (!user) {
+            return {
+              ok: false,
+              error: "This user does not exist.",
+            };
+          }
+          room = await client.room.create({
+            data: {
+              users: {
+                connect: [
+                  {
+                    id: userId,
+                  },
+                  {
+                    id: loggedInUser.id,
+                  },
+                ],
+              },
+            },
+          });
+        } else if (roomId) {
+          room = await client.room.findUnique({
+            where: {
+              id: roomId,
+            },
+            select: {
+              id: true,
+            },
+          });
+          if (!room) {
+            return {
+              ok: false,
+              error: "Room not found.",
+            };
+          }
+        }
+        const message = await client.message.create({
+          data: {
+            payload,
+            room: {
+              connect: {
+                id: room.id,
+              },
+            },
+            user: {
+              connect: {
                 id: loggedInUser.id,
               },
             },
           },
-        },
-        select: {
-          id: true,
-        },
-      });
-      if (!message) {
+        });
+        pubsub.publish(NEW_MESSAGE, { roomUpdates: { ...message } });
         return {
-          ok: false,
-          error: "Message not found.",
+          ok: true,
         };
       }
-      await client.message.update({
-        where: {
-          id,
-        },
-        data: {
-          read: true,
-        },
-      });
-      return {
-        ok: true,
-      };
-    }),
+    ),
   },
 };
--- server.js
+++ server.js
@@ -1,31 +1,62 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server-express";
-import { graphqlUploadExpress } from "graphql-upload";
+import { createServer } from "http";
+import { execute, subscribe } from "graphql";
+import { SubscriptionServer } from "subscriptions-transport-ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 import express from "express";
 import logger from "morgan";
-import { typeDefs, resolvers } from "./schema.js";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
-
+import { graphqlUploadExpress } from "graphql-upload";
 
 const PORT = process.env.PORT;
-
 const startServer = async () => {
-  const apollo = new ApolloServer({
+  const app = express();
+  const httpServer = createServer(app);
+  const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
-    },
   });
 
-  await apollo.start();
-  const app = express();
-  app.use(graphqlUploadExpress(), logger("tiny"));
-  apollo.applyMiddleware({ app });
-  app.use("/static", express.static("uploads"));
-  await new Promise((func) => app.listen({ port: PORT }, func));
-  console.log(`🚀 Server: http://localhost:${PORT}${apollo.graphqlPath}`);
+  const subscriptionServer = SubscriptionServer.create(
+      { schema, execute, subscribe },
+      { server: httpServer }
+  ); //subscriptions 서버를 만듭니다.
+
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    if (req) {
+      return {
+    loggedInUser: await getUser(req.headers.token),
+    };
+  }
+},
+  plugins: [
+  {
+  async serverWillStart() {
+    return {
+    async drainServer() {
+    subscriptionServer.close();
+    },
+    };  
+  },
+  },
+  ],
+  });
+
+await server.start();
+
+app.use(logger("tiny"));
+app.use(graphqlUploadExpress());
+server.applyMiddleware({ app });
+app.use("/static", express.static("uploads"));
+
+httpServer.listen(PORT, () =>
+console.log(
+`🚀 Server is running on http://localhost:${PORT}${server.graphqlPath} ✅`
+)
+);
 };
 startServer();
